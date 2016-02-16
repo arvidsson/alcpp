a4d2a588d2f910636b8febf0e520286bb5c1fdec
arvidsson <arvidsson@gmail.com>
2016-02-16 18:29:28 +0100

    documented keyboard and mouse states

--- alcpp/input/KeyboardState.hpp
+++ alcpp/input/KeyboardState.hpp
@@ -5,11 +5,23 @@
 namespace alcpp
 {
 
+/*
+    Holds snapshot of keyboard state. Must be polled every frame.
+*/
 class KeyboardState
 {
 public:
+    /*
+        Updates the keyboard state.
+    */
     void Poll();
+
+    /*
+        Returns true if the key is held down.
+    */
     bool IsKeyDown(int keycode);
+
+    // TODO: get display which had focus on keypress
 
 private:
     ALLEGRO_KEYBOARD_STATE state;
--- alcpp/input/MouseState.hpp
+++ alcpp/input/MouseState.hpp
@@ -5,15 +5,37 @@
 namespace alcpp
 {
 
+/*
+    Holds snapshot of mouse state. Must be polled every frame.
+*/
 class MouseState
 {
 public:
+    /*
+        Updates the keyboard state.
+    */
     void Poll();
+
+    /*
+        Returns true if the mouse button is held down.
+    */
     bool IsButtonDown(int button);
+
+    /*
+        Returns the mouse position.
+    */
     int GetX() const;
     int GetY() const;
+
+    /*
+        Returns the mouse wheel position.
+    */
     int GetZ() const;
     int GetW() const;
+
+    /*
+        Returns the mouse wheel pressure.
+    */
     float GetPressure() const;
 
 private:
