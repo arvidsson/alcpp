fc1fb343b4194e2d2dd78e2414c12315ae283125
arvidsson <arvidsson@gmail.com>
2016-02-16 21:42:44 +0100

    added SharedPtr

---
+++ alcpp/util/SharedPtr.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+
+namespace alcpp
+{
+
+template <class T>
+class SharedPtr : public std::shared_ptr<T>
+{
+protected:
+    static void nullDeleter(T*) {}
+    SharedPtr() {}
+    SharedPtr(const std::shared_ptr<T> &ptr) : std::shared_ptr<T>(ptr) {}
+    SharedPtr(T *ptr) : std::shared_ptr<T>(ptr) {}
+    template <class D> SharedPtr(T *ptr, D deleter) : std::shared_ptr<T>(ptr, deleter) {}
+    template <class D> SharedPtr(T *ptr, D deleter, bool retain) : std::shared_ptr<T>(ptr, retain ? deleter : nullDeleter) {}
+
+public:
+    T* Get() const { return get(); }
+};
+
+}
