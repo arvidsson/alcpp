67ad42a948df510d74986b48ac103da8fe2b9ff6
arvidsson <arvidsson@gmail.com>
2016-02-16 21:42:55 +0100

    minor fix

--- alcpp.hpp
+++ alcpp.hpp
@@ -2,17 +2,22 @@
 
 #include "alcpp/System.hpp"
 
+#include "alcpp/event/Event.hpp"
 #include "alcpp/event/EventQueue.hpp"
 #include "alcpp/event/EventSource.hpp"
-#include "alcpp/event/Event.hpp"
 
+#include "alcpp/graphics/Shader.hpp"
+#include "alcpp/graphics/Transform.hpp"
+
+#include "alcpp/input/Key.hpp"
+#include "alcpp/input/Keyboard.hpp"
 #include "alcpp/input/KeyboardState.hpp"
+#include "alcpp/input/Mouse.hpp"
+#include "alcpp/input/MouseButton.hpp"
 #include "alcpp/input/MouseState.hpp"
 
 #include "alcpp/time/Time.hpp"
 #include "alcpp/time/Timer.hpp"
 
-#include "alcpp/graphics/Shader.hpp"
-#include "alcpp/graphics/Transform.hpp"
-
+#include "alcpp/util/SharedPtr.hpp"
 #include "alcpp/util/ToIntegral.hpp"
--- alcpp/event/EventSource.hpp
+++ alcpp/event/EventSource.hpp
@@ -1,20 +1,15 @@
 #pragma once
 
+#include "../util/SharedPtr.hpp"
 #include <allegro5/allegro.h>
 
 namespace alcpp
 {
 
-class EventSource
+class EventSource : public SharedPtr<ALLEGRO_EVENT_SOURCE>
 {
 public:
-    EventSource(ALLEGRO_EVENT_SOURCE *eventSource) : source(eventSource) {}
-    ~EventSource() {}
-
-    ALLEGRO_EVENT_SOURCE* Get() { return source; }
-
-private:
-    ALLEGRO_EVENT_SOURCE *source = nullptr;
+    EventSource(ALLEGRO_EVENT_SOURCE *eventSource) : SharedPtr(eventSource, nullDeleter) {}
 };
 
 }
